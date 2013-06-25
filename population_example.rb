83fc06550e968e69e4e26386d39f0dda02b1c5bd
bennacer <bennacer860@gmail.com>
2013-06-25 10:17:06 -0400

    change example path

--- population_example.rb
+++ population_example.rb
@@ -1,5 +1,5 @@
 require 'benchmark'
 require './benford'
 
-time = Benchmark.measure { Benford.new("population_example.csv","Population") }
+time = Benchmark.measure { Benford.new("data/population_example.csv","Population") }
 puts "\nBenchmark: #{time * 1000}"
